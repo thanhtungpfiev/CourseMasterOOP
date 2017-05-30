83a4ddda0b2c94d85a13a4a00866b4896f1f6dfd
thanhtungpfiev <thanhtungpfiev@gmail.com>
2017-05-30 16:00:11 +0700

    Section 1 Lecture 3 zoo

---
+++ Prerequisite_OOP/src/zoo/Fish.java
@@ -0,0 +1,16 @@
+/**
+ * tungdt9
+ * May 30, 2017
+ * TODO
+ */
+package zoo;
+
+/**
+ * @author tungdt9
+ *
+ */
+public class Fish {
+	public void swim() {
+		System.out.println("Swimming...");
+	}
+}
---
+++ Prerequisite_OOP/src/zoo/Zoo.java
@@ -0,0 +1,25 @@
+/**
+ * tungdt9
+ * May 30, 2017
+ * TODO
+ */
+package zoo;
+
+/**
+ * @author tungdt9
+ *
+ */
+public class Zoo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Fish fish = new Fish();
+		fish.swim();
+		Bird bird = new Bird();
+		bird.fly();
+	}
+
+}
