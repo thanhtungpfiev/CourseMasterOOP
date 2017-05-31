7b847c879f88695a818d7c672fd1d91102621fb2
thanhtungpfiev <thanhtungpfiev@gmail.com>
2017-05-31 16:55:26 +0700

    Section 1 Lecture 5 add zoo

---
+++ Prerequisite_OOP_2/src/Zoo.java
@@ -0,0 +1,22 @@
+/**
+ * tungdt9
+ * May 30, 2017
+ * TODO
+ */
+
+
+/**
+ * @author tungdt9
+ *
+ */
+public class Zoo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Animal animal = new Animal(24, "M", 72);
+		animal.eat();
+	}
+}
