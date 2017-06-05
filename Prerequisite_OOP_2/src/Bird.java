53905c0fd1c0db438a7821908d4f4b7de781c239
thanhtungpfiev <thanhtungpfiev@gmail.com>
2017-06-05 11:37:22 +0700

    Section 1 Lecture 7 inheritance

---
+++ Prerequisite_OOP_2/src/Bird.java
@@ -0,0 +1,26 @@
+/**
+ * tungdt9
+ * Jun 5, 2017
+ * TODO
+ */
+
+/**
+ * @author tungdt9
+ *
+ */
+public class Bird extends Animal {
+
+	/**
+	 * @param age
+	 * @param gender
+	 * @param weightInPounds
+	 */
+	public Bird(int age, String gender, double weightInPounds) {
+		super(age, gender, weightInPounds);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void fly() {
+		System.out.println("Flying...");
+	}
+}
--- Prerequisite_OOP_2/src/Zoo.java
+++ Prerequisite_OOP_2/src/Zoo.java
@@ -16,7 +16,11 @@ public class Zoo {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Animal animal = new Animal(24, "M", 72);
-		animal.eat();
+		Animal animal1 = new Animal(24, "M", 72);
+		animal1.eat();
+		
+		Bird bird1 = new Bird(3, "F", 12);
+		bird1.eat();
+		bird1.fly();
 	}
 }
